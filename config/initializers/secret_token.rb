fe7c17e259b21737392224079d5e936c615cdd62
Jonan Scheffler <jonanscheffler@gmail.com>
2012-06-06 11:15:07 -0400

    Rails Attack

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SandboxA::Application.config.secret_token = '049886525678f51e0ee854091c18d5b63248611a94158d223a7fe3920f8c640fffc35f80974d93c7f591d29cf1033f20e634bea1d1ccfaeac3ec3220070eb1f9'
